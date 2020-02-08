cc0c0a17b2efe471f4db865c1e0c4c3183a6f5a5
Lee Dain <dlekdls02@naver.com>
2020-02-09 03:51:40 +0900

    dp

---
+++ programmers/level3/거스름돈.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include<iostream>
+using namespace std;
+
+/*
+j원일때 i라는 돈으로 낼 수 있는 가지수 누적합
+   1 2 3 4 5
+1  1 1 1 1 1
+2  x 2 2 3 3
+5  x x x x 4
+*/
+int solution(int n, vector<int> money) {
+    int answer = 0;
+    vector<int> v(n+1, 0);
+    
+    v[0] = 1;
+    for(int i=0; i<money.size(); i++){
+        for(int j=1; j<=n; j++){
+            if(j-money[i]>=0){
+                v[j] +=v[j-money[i]];
+            }
+        }
+    }
+    return v[n];
+}
